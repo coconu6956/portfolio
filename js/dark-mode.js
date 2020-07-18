5f04650592770cfdb137fcd853b9730c2a6ac835
chaeeun <btd.chaeeun@gmail.com>
2020-07-18 19:19:55 +0900

    수정 중

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,18 +1,21 @@
-
 function userTheme() {
- window.__THEME_MODE = 'light';
- if (!!window.matchMedia) {
-  window.__THEME_MODE = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'auto';
- }
-  document.getElementsByTagName('html')[0].classList[window.__THEME_MODE === 'dark' ? 'add' : 'remove']('darkmode');
+    window.__THEME_MODE = 'light';
+    if (!!window.matchMedia) {
+        window.__THEME_MODE = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'auto';
+    }
+    document.getElementsByTagName('html')[0].classList[window.__THEME_MODE === 'dark' ? 'add' : 'remove']('darkmode');
 }
 if (!!window.matchMedia) {
  ['light', 'dark'].forEach(mode => {
-  window.matchMedia(`(prefers-color-scheme: ${mode})`).addListener(e => {
-   if(!!e.matches) {
-    userTheme();
-   }
-  });  
- });
+        window.matchMedia(`(prefers-color-scheme: ${mode})`).addListener(e => {
+            if (!!e.matches) {
+                userTheme();
+            }
+        });
+    });
 }
 userTheme();
+
+$(document).ready(function () {
+    
+});
