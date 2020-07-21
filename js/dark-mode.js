4dd9a8f83b2b261718d832315f1a24a5155c94f1
chaeeun <btd.chaeeun@gmail.com>
2020-07-22 03:31:56 +0900

    다크모드 테스트

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,12 +1,12 @@
 function userTheme(toggle = false) {
-    //let userMode = localStorage.userThemeMode || 'auto';
-    let userMode = localStorage.userThemeMode;
+    let userMode = localStorage.userThemeMode || 'auto';
+    //let userMode = localStorage.userThemeMode;
     const osMode = !!window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-    //console.log(osMode);
-    //console.log(userMode);
+    console.log(osMode);
+    console.log(userMode);
     if (toggle) {
         switch (userMode) {
-            case 'light':
+            case 'auto':
                 userMode = 'dark';
                 $('.thema_btn i').removeClass("fas fa-sun").addClass('fas fa-moon');   
                 break;
@@ -15,11 +15,11 @@ function userTheme(toggle = false) {
                 $('.thema_btn i').removeClass("fas fa-moon").addClass('fas fa-sun'); 
                 break;
             default:
-                userMode = 'light';
+                userMode = 'auto';
         }
         localStorage.userThemeMode = userMode;
     }
-    //console.log(`current mode : ${userMode}`);
+    console.log(`current mode : ${userMode}`);
     window.__THEME_MODE = userMode === 'auto' ? osMode : userMode;
     document.getElementsByTagName('html')[0].classList[window.__THEME_MODE === 'dark' ? 'add' : 'remove']('darkmode');
 }
@@ -33,3 +33,9 @@ if (!!window.matchMedia) {
     });
 }
 userTheme();
+
+$(document).ready(function(){
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        $('.thema_btn i').removeClass("fas fa-sun").addClass('fas fa-moon');  
+    }
+});
